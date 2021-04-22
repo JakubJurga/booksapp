809b30f9785f7f120caff0dbe907bc018311018d
Jakub Jurga <>
2021-04-22 10:30:15 +0100

    Add determineRating

--- src/js/script.js
+++ src/js/script.js
@@ -21,9 +21,20 @@
 
 
   function render () {
-    for (const id of dataSource.books){
+    for (const book of dataSource.books){
+      const ratingBgc = determineRatingBgc(book.rating);
+      const ratingWidth = book.ratingBgc * 10;
       const booklist = document.querySelector(select.booklist);
-      const generatedHTML = templates.template(id);
+
+      const generatedHTML = templates.template({
+        id: book.id,
+        price: book.price,
+        name: book.name,
+        image: book.image,
+        rating: book.rating,
+        ratingBgc,
+        ratingWidth,
+      });
 
       const elementHTML = utils.createDOMFromHTML(generatedHTML);
 
@@ -74,34 +85,48 @@
         const indexOfFilter = filters.indexOf(event.target.value);
         filters.splice(indexOfFilter, 1);
       }
-       filterBooks();
+      filterBooks();
     });
 
 
 
   }
 
-function filterBooks(){
-  for(let book of dataSource.books){
-    let shouldBeHidden = false;
+  function filterBooks(){
+    for(let book of dataSource.books){
+      let shouldBeHidden = false;
 
-    for(const filter of filters){
-      if(!book.details[filter]){
-        shouldBeHidden = true;
-        break;
+      for(const filter of filters){
+        if(!book.details[filter]){
+          shouldBeHidden = true;
+          break;
+        }
+      }
+
+      const idOfBook = book.id;
+      const itemImage = document.querySelector('.book__image[data-id="'+idOfBook+'"]');
+
+      if(shouldBeHidden == true){
+        itemImage.classList.add('hidden');
+      } else if (shouldBeHidden == false){
+        itemImage.classList.remove('hidden');
       }
     }
-
-    const idOfBook = book.id;
-    const itemImage = document.querySelector('.book__image[data-id="'+idOfBook+'"]');
-
-    if(shouldBeHidden == true){
-     itemImage.classList.add('hidden');
-   } else if (shouldBeHidden == false){
-     itemImage.classList.remove('hidden');
-   }
   }
-}
+
+  function determineRatingBgc(rating){
+    let background = '';
+    if (rating < 6){
+      background = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
+    } else if (rating > 6 && rating <= 8) {
+      background = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+    } else if(rating > 8 && rating <= 9) {
+      background = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+    } else if(rating > 9){
+      background = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+    }
+    return background;
+  }
 
 
   initActions();
